8deb013c3584bf1ff58854276ab091640ff40c6f
Urabash <122707123+Urabash@users.noreply.github.com>
2023-01-19 20:58:20 +0100

    Update discord_update.sh

--- discord_update.sh
+++ discord_update.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
-POST=$(curl -sI 'https://discord.com/api/download?platform=linux&format=deb' | grep location | sed 's/\r//g') # Get Url from Discord API  
-filename=$(echo $POST | cut -c55-) # Get Filename of latest version
-url=$(echo $POST | cut -c11-) # Get url for file location
-last_version=$(echo $POST | cut -c63-68) # Get the last version number of Discord.
 
-if dpkg -s discord &> /dev/null; then # Check if Discord are installed.
-    current_version=$(dpkg -s discord | grep Version | cut -c10-) # Get installed version
-    if [ "$current_version" != "$last_version" ]; then # Compare version
-        wget "$url" # Download new update
-        dpkg -i $filename # install update
-        rm $filename # delete .deb file
-        echo "Discord version $current_version updated to $last_version" >> log.txt  
+# Get url for the latest version of Discord from the Discord API
+POST=$(curl -sI 'https://discord.com/api/download?platform=linux&format=deb' | grep location | sed 's/\r//g') 
+
+# Get the filename of the latest version
+filename=$(echo $POST | cut -c55-) 
+
+# Get the url for the file location
+url=$(echo $POST | cut -c11-) 
+
+# Get the last version number of Discord
+last_version=$(echo $POST | cut -c63-68) 
+
+# Initialize the counter
+init=false
+
+# Loop until the script is interrupted
+while true
+do
+    # Check if Discord is running or if the counter is equal to 0
+    if pgrep Discord > /dev/null || [ $init == false ] ; then
+        
+        # Check if Discord is installed
+        if dpkg -s discord &> /dev/null; then 
+            
+            # Get the current version of Discord that is installed
+            current_version=$(dpkg -s discord | grep Version | cut -c10-) 
+            
+            # Compare the current version to the last version
+            if [ "$current_version" != "$last_version" ]; then 
+                
+                # Kill the running Discord process
+                kill $(pgrep Discord)
+                
+                # Download the new update
+                wget "$url" 
+                
+                # Install the update
+                dpkg -i $filename 
+                
+                # Delete the .deb file
+                rm $filename 
+                
+                # Log the update in a log file
+                echo "Discord version $current_version updated to $last_version"
+			else
+				echo "Nothing to do."  
+            fi
+        else
+            # If Discord is not installed, print a message
+            echo "Discord is not installed"
+        fi
     fi
-else
-    echo "Discord is not installed"
-fi
+    # Increase the counter
+    init=true
+done
